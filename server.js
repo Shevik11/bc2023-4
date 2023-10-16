cf5d503a17f61c046a5efce8c786d7db37ca3864
Shevik11 <144562073+Shevik11@users.noreply.github.com>
2023-10-16 14:31:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 const http = require('http'); // http server
 const fs = require('fs'); // fs - file system
+const xml = require('fast-xml-parser'); //
 
 const server = http.createServer((req, res) => { // Створюємо HTTP сервер і визначаємо обробник запитів.
   if (req.method === 'GET' && req.url === '/') { // Перевіряємо, чи запит є GET-запитом і чи він адресований кореневому шляху ('/').
     fs.readFile('data.xml', 'utf-8', (err, data) => { 
         //parse
-        const maxRate = findMaxRate(data); 
-
-
-        const responseXml = `<data><max_rate>${maxRate}</max_rate></data>`; 
-        res.end(responseXml); 
+        const maxRate = findMaxRate(data);
+        console.log(maxRate);
+         
+      const builder = new xml.XMLBuilder();
+      const xmlStr = builder.build({"data" : {"max_rate" : maxRate}});
+        res.end(xmlStr); 
     });
   } 
 });
 
 function findMaxRate(xmlData) { 
 
-  const rates = xmlData.match(/<rate>([\d.]+)<\/rate>/g); // regular expression
+  const parser = new xml.XMLParser();
+  const rates = parser.parse(xmlData)["exchange"]["currency"]
 
   let maxRate = 0; 
 
   for (const rate of rates) { 
-    const rateValue = parseFloat(rate.match(/<rate>([\d.]+)<\/rate>/)[1]); // [1] - denote value
+    const rateValue = rate["rate"] // [1] - denote value
 
     if (rateValue > maxRate) { 
       maxRate = rateValue; 
